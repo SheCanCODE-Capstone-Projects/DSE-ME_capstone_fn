79a4c114949addde5fe68d29562e4cc88e89a631
Ange290 <uange209@gmail.com>
2025-12-03 16:55:18 +0200

    remove unnecessary tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Google from "@/components/ui/Google";
+import Password from "@/components/ui/Password";
+// import Google from "@/components/ui/Google";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
      
-    <Google/>
+    <Password/>
+    {/* <Google/> */}
     </div>
   );
 }
